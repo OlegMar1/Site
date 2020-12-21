b5dfed0e23a32ba60ca8ff53c4c7365ce9fb234e
Olegmar1 <mixassv11@gmail.com>
2020-12-21 20:56:09 +0200

    test

---
+++ js/buttons.js
@@ -0,0 +1,39 @@
+function openMenu(){
+  document.getElementById("sidebar").classList.toggle('active');
+  document.getElementById("head").classList.toggle('head-with-sidebar');
+  document.getElementById("main").classList.toggle('main-with-sidebar');
+    document.querySelector(".graph").classList.toggle('graph-with-sidebar');
+}
+
+function openPoints(e){
+  let coll = e.target;
+  let content = coll.nextElementSibling;
+
+  coll.classList.toggle('activated'); //Повернути стрілку
+
+  if(content.style.maxHeight){        //Розкрити меню
+    content.style.maxHeight = null;
+  } else{
+    content.style.maxHeight = content.scrollHeight + 'px';
+  }
+
+  /*let coll = document.getElementsByClassName('over_item');
+  for(let i = 0; i < coll.length; i++){
+    coll[i].addEventListener('click', function(){
+      // this.classList.toggle('active');
+      let content = this.nextElementSibling;
+      if(content.style.maxHeight){
+        content.style.maxHeight = null;
+      } else{
+        content.style.maxHeight = content.scrollHeight + 'px';
+      }
+    })
+  }*/
+
+/*  let arrow = document.getElementsByClassName('arrow');
+  for(let p = 0; i < arrow.length; p++){
+    arrow[p].addEventListener('click', function(){
+      this.classList.toggle('active');
+    })
+  }*/
+}
